b984f0ac66216583d692672ee9d5e4190288b102
honghui <252860883@qq.com>
2018-10-28 18:15:08 +0800

    添加参数

--- src/draw/drawPlace.js
+++ src/draw/drawPlace.js
@@ -15,16 +15,19 @@ export default class DrawGame {
         // 绘图相关的属性
         this.x = 40;
         this.y = 200;
-        this.col = 3;
-        this.row = 3;
+        this.col = 4;
+        this.row = 4;
         this.gab = 20;
         this.tWidth = 750 - 2 * this.x;//桌面宽度
         this.iWidth = (this.tWidth - (this.row + 1) * this.gab) / this.row;//单个的高度
         this.tHeight = this.iWidth * this.col + (this.col + 1) * this.gab;
 
+        this.spItem;
+
         // 游戏数据
         this.num = '011010011001101010001010';
         this.arr = [];
+        this.pNow = [0, 0];
 
         // 游戏交互
         this.startP = [];
@@ -34,7 +37,16 @@ export default class DrawGame {
     }
     init() {
         console.log('执行init');
+        for (var i = 0; i < this.row; i++) {
+            this.arr[i] = [];
+            for (var j = 0; j < this.col; j++) {
+                this.arr[i][j] = this.num.slice(i * this.row + j, i * this.row + j + 1)
+            }
+        }
+
         this.drawPlace()
+        this.spItem = new Laya.Sprite();
+        Laya.stage.addChild(this.spItem);
         this.drawTable()
     }
     // 画背景图
@@ -58,21 +70,24 @@ export default class DrawGame {
     }
 
     // 画初始宫格
-    drawTable(col, row) {
-        var spItem = new Laya.Sprite();
-        Laya.stage.addChild(spItem);
+    drawTable() {
+        this.spItem.graphics.clear();
         for (var i = 0; i < this.row; i++) {
-            this.arr[i] = [];
             for (var j = 0; j < this.col; j++) {
-                this.arr[i][j] = this.num.slice(i * this.row + j, i * this.row + j + 1)
                 let x = this.x + i * (this.iWidth + this.gab) + this.gab, y = this.y + j * (this.iWidth + this.gab) + this.gab;
-                // console.log(x, y);
-                let color = this.arr[i][j] == 0 ? '#efb7dd' : '#666'
-                DRAW.drawRoundedRectangle(spItem, x, y, this.iWidth, this.iWidth, 15, '#efb7dd')
+                let color = "";
+                if (i == this.pNow[0] && j == this.pNow[1]) {
+                    color = "eb6877"; //滑动色块的值
+                } else {
+                    color = this.arr[i][j] == 0 ? '#efb7dd' : '#8952a3';
+                }
+                DRAW.drawRoundedRectangle(this.spItem, x, y, this.iWidth, this.iWidth, 15, color)
             }
         }
         console.log(this.arr)
     }
+
+
 
     /**
      * 键盘事件
@@ -88,6 +103,7 @@ export default class DrawGame {
             case 40: direction = 'down'; break;
         }
         console.log('direction is ' + direction)
+        this.moveBlock(direction)
     }
 
     /**
@@ -95,7 +111,6 @@ export default class DrawGame {
      */
     onMouseDown(params) {
         this.startP = [Laya.stage.mouseX, Laya.stage.mouseY];
-        // console.log(this.startP);
     }
     onMouseUp() {
         let endP = [Laya.stage.mouseX, Laya.stage.mouseY];
@@ -108,6 +123,7 @@ export default class DrawGame {
             direction = Math.abs(moveY) > 50 && moveY > 0 ? 'down' : 'top';
         }
         console.log(direction, moveX, moveY)
+        this.moveBlock(direction)
     }
     onMouseMove(e) {
         // console.log(e)
@@ -118,4 +134,51 @@ export default class DrawGame {
     /**
      * 逻辑处理
      */
+
+    moveBlock(direction) {
+        console.log("移动");
+        let a = this.pNow[0],
+            b = this.pNow[1];
+        // this.arr[a][b] = this.arr[a][b] == "1" ? "0" : "1";
+        if (direction == "down") {
+            if (this.pNow[1] >= this.col - 1) {
+                return;
+            }
+            this.pNow[1]++;
+        } else if (direction == "top") {
+            if (this.pNow[1] <= 0) {
+                return;
+            }
+            this.pNow[1]--;
+        } else if (direction == "right") {
+            if (this.pNow[0] >= this.row - 1) {
+                return;
+            }
+            this.pNow[0]++;
+        } else if (direction == "left") {
+            if (this.pNow[0] <= 0) {
+                return;
+            }
+            this.pNow[0]--;
+        }
+        let i = this.pNow[0],
+            j = this.pNow[1];
+        // console.log(this.arr)
+        // if (this.arr[i][j].isUsed) {
+        //     this.pNow[0] = b;
+        //     this.pNow[1] = a;
+        //     return;
+        // }
+        this.arr[i][j] = this.arr[i][j] == "1" ? "0" : "1";
+        // this.arr[i][j].isUsed = this.arr[i][j].num == "1" ? true : false;
+        // this.arr[i+1][j] = this.arr[i+1][j] == "1" ? "0" : "1";
+        console.log(this.arr[i][j]);
+        // this.arr[1][0] = "1";
+        // 为了视图更新
+        // let temp = this.arr;
+        // this.arr = [];
+        // this.arr = temp;
+        // this.judgeSuccess();
+        this.drawTable();
+    }
 }
